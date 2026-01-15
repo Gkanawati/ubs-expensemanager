ac239c67f54d9b629d3bcc56640a73089d6f89ee
exd02 <eliamxd1337@gmail.com>
2026-01-14 21:57:25 -0300

    fix: changed utc time

--- backend/src/main/java/com/ubs/expensemanager/ExpenseManagerApplication.java
+++ backend/src/main/java/com/ubs/expensemanager/ExpenseManagerApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class ExpenseManagerApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT-03:00"));
         SpringApplication.run(ExpenseManagerApplication.class, args);
     }
 }
